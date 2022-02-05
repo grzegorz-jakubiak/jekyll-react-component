d2063db42ea58c029d9563f9edece2f4dcff0bf3
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2022-02-05 15:04:49 +0100

    Version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## [Unreleased]
 
+## [0.1.1] - 2022-02-05
+- Add import map only if a page defines react_component
+
 ## [0.1.0] - 2021-08-28
 
 - Initial release
--- lib/jekyll-react-component.rb
+++ lib/jekyll-react-component.rb
@@ -5,25 +5,36 @@ require 'nokogiri'
 require_relative 'jekyll-react-component/react_component'
 require_relative 'jekyll-react-component/version'
 
-Jekyll::Hooks.register [:pages, :posts], :post_render do |page|
-  html = page.output
-  doc = Nokogiri::HTML(html)
-  head = doc.at_css('head')
-  unless head.nil?
-    head << <<~CODE
-      <script async src="https://unpkg.com/es-module-shims@0.12.8/dist/es-module-shims.js"></script>
-      <script type="importmap">
-        {
-          "imports": {
-            "react": "https://esm.sh/react",
-            "react-dom": "https://esm.sh/react-dom",
-            "htm": "https://esm.sh/htm",
-            "assets/": "/assets/"
-          }
-        }
-      </script>
-    CODE
-  end
+$react_component_pages_id = []
 
-  page.output = doc.to_html
+Jekyll::Hooks.register [:pages, :posts], :pre_render do |page|
+  if page.content.match?(/\{% react_component .+ %\}(.|\n)*\{% endreact_component %\}/)
+    $react_component_pages_id << page.object_id
+  end
+end
+
+Jekyll::Hooks.register [:pages, :posts], :post_render do |page|
+  if $react_component_pages_id.include?(page.object_id)
+    html = page.output
+    doc = Nokogiri::HTML(html)
+    head = doc.at_css('head')
+    unless head.nil?
+      head << <<~CODE
+        <script async src="https://unpkg.com/es-module-shims@0.12.8/dist/es-module-shims.js"></script>
+        <script type="importmap">
+          {
+            "imports": {
+              "react": "https://esm.sh/react",
+              "react-dom": "https://esm.sh/react-dom",
+              "htm": "https://esm.sh/htm",
+              "assets/": "/assets/"
+            }
+          }
+        </script>
+      CODE
+    end
+    
+    $react_component_pages_id.clear
+    page.output = doc.to_html
+  end
 end
